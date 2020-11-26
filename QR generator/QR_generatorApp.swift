9209f3a420ddc243e7e41b78f8629c9c0208f021
Luca Bruzzone <spam@unsigned.it>
2020-11-26 12:43:13 +0100

    Initial Commit

---
+++ QR generator/QR_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_generatorApp.swift
+//  QR generator
+//
+//  Created by lbruzzone on 26.11.20.
+//
+
+import SwiftUI
+
+@main
+struct QR_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
